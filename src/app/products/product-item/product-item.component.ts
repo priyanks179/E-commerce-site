ec9ca64f2ee60afc4b417ef8278f11f05b58353b
amiya-1998 <amiya.tripathy1998@gmail.com>
2020-05-01 13:38:28 +0530

    minor bug fix

--- src/app/products/product-item/product-item.component.ts
+++ src/app/products/product-item/product-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/shared/product.model';
 import { DataStorageService } from 'src/app/shared/data-storage.service';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/authentication/auth.service';
 
 @Component({
   selector: 'app-product-item',
@@ -12,20 +13,38 @@ export class ProductItemComponent implements OnInit {
   @Input() product: Product;
   constructor(
     private dataStorageService: DataStorageService,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {}
 
   onAddWishList() {
-    this.dataStorageService.addToWishList(this.product.id).subscribe();
+    if (this.authService.getUsername()) {
+      this.dataStorageService.addToWishList(this.product.id).subscribe();
+    } else {
+      const btn: HTMLElement = document.querySelector('#login-btn');
+      btn.click();
+    }
   }
 
   onAddCart() {
-    this.dataStorageService.addToCart(this.product.id).subscribe();
+    if (this.authService.getUsername()) {
+      this.dataStorageService.addToCart(this.product.id).subscribe();
+    } else {
+      const btn: HTMLElement = document.querySelector('#login-btn');
+      btn.click();
+    }
   }
 
   onCheckOut() {
-    this.router.navigate(['/cart']);
+    if (this.authService.getUsername()) {
+      this.dataStorageService.addToCart(this.product.id).subscribe(() => {
+        this.router.navigate(['/cart']);
+      });
+    } else {
+      const btn: HTMLElement = document.querySelector('#login-btn');
+      btn.click();
+    }
   }
 }
